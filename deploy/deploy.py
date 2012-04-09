08ed32bf98f234155a1e1e904a500184e8a1ec36
doc_bacardi <devnull@localhost>
2012-04-09 21:58:29 +0000

    transform xml tree into dict for easier filtering

--- deploy/deploy.py
+++ deploy/deploy.py
@@ -6,6 +6,7 @@ import httplib2
 import optparse
 import os.path
 import sys
+import xml.dom.minidom
 
 
 import deploy_version
@@ -49,22 +50,15 @@ def generate_pom(strSrcPath, strGroupID, strArtifactID, strPackaging, strRevisio
 	return tTemplate.safe_substitute(aSubstitute)
 
 
-def execute_scan(tXml):
-	# Create the rest driver for httplib2.
-	tRestDriver = rest_driver_httplib2.RestDriver()
+def read_xml(strFileName):
+	# Read the artifact list.
+	tXml = ElementTree()
+	tXml.parse(strFileName)
 
-	# Create the repository driver.
-	tRepository = repository_driver_nexus.RepositoryDriver(tRestDriver, 'http://nexus.netx01.hilscher.local/')
+	aArtifacts = dict({})
 
-	atAllTargetNodes = tXml.findall('Project/Targets/Target')
-	sizAllTargetNodes = len(atAllTargetNodes)
-	print 'Found %d artifacts.' % sizAllTargetNodes
-
-	# Process all Target nodes.
-	sizNodeCnt = 0
-	for tNode in atAllTargetNodes:
-		sizNodeCnt += 1
-		print 'Processing artifact %d/%d (%d%%)' % (sizNodeCnt, sizAllTargetNodes, sizNodeCnt*100/sizAllTargetNodes)
+	for tNode in tXml.findall('Project/Targets/Target'):
+		aAttrib = dict({})
 		strValue = tNode.get('file')
 		if strValue==None:
 			raise Exception('One of the Target nodes has no file attribute!')
@@ -72,6 +66,11 @@ def execute_scan(tXml):
 			strFile = strValue.strip()
 			if strFile=='':
 				raise Exception('One of the Target nodes has an empty file attribute!')
+			aAttrib['file'] = strFile
+
+		strValue = tNode.get('selected', 'False')
+		bSelected = bool(strValue.strip())
+		aAttrib['selected'] = bSelected
 
 		strValue = tNode.findtext('ArtifactID')
 		if strValue==None:
@@ -80,6 +79,7 @@ def execute_scan(tXml):
 			strArtifactID = strValue.strip()
 			if strArtifactID=='':
 				raise Exception('One of the Target nodes has an empty ArtifactID child!')
+			aAttrib['aid'] = strArtifactID
 
 		strValue = tNode.findtext('GroupID')
 		if strValue==None:
@@ -88,6 +88,7 @@ def execute_scan(tXml):
 			strGroupID = strValue.strip()
 			if strGroupID=='':
 				raise Exception('One of the Target nodes has an empty GroupID child!')
+			aAttrib['gid'] = strGroupID
 
 		strValue = tNode.findtext('Packaging')
 		if strValue==None:
@@ -96,27 +97,95 @@ def execute_scan(tXml):
 			strPackaging = strValue.strip()
 			if strPackaging=='':
 				raise Exception('One of the Target nodes has an empty Packaging child!')
+			aAttrib['packaging'] = strPackaging
 
+		# Read all versions.
+		aVersions = dict({})
+		for tVersionNode in tNode.findall('version'):
+			strVersion = tVersionNode.text.strip()
+			if strVersion=='':
+				raise Exception('One of the Target nodes has an empty version child!')
+			if strVersion in aVersions:
+				raise Exception('Double version!')
+			bMatch = bool(tVersionNode.get('match'))
+			aVersions[strVersion] = bMatch
+		aAttrib['versions'] = aVersions
+
+		strKey = '%s.%s' % (strGroupID, strArtifactID)
+		if strKey in aArtifacts:
+			raise Exception('Double key %s!' % strKey)
+		aArtifacts[strKey] = aAttrib
+
+	return aArtifacts
+
+
+def write_xml(strFileName, aArtifacts):
+	tXml = xml.dom.minidom.getDOMImplementation().createDocument(None, "Artifacts", None)
+	tNode_Project = tXml.documentElement.appendChild(tXml.createElement('Project'))
+	tNode_Targets = tNode_Project.appendChild(tXml.createElement('Targets'))
+
+	# Loop over all artifacts.
+	for aAttrib in aArtifacts.itervalues():
+		tNode_Target = tNode_Targets.appendChild(tXml.createElement('Target'))
+		tNode_Target.setAttribute('file', aAttrib['file'])
+		tNode_Target.setAttribute('selected', str(aAttrib['selected']))
+
+		# Create ArtifactID, GroupID and Packaging children.
+		tNode_ArtifactID = tNode_Target.appendChild(tXml.createElement('ArtifactID'))
+		tNode_ArtifactID.appendChild(tXml.createTextNode(aAttrib['aid']))
+
+		tNode_GroupID = tNode_Target.appendChild(tXml.createElement('GroupID'))
+		tNode_GroupID.appendChild(tXml.createTextNode(aAttrib['gid']))
+
+		tNode_Packaging = tNode_Target.appendChild(tXml.createElement('Packaging'))
+		tNode_Packaging.appendChild(tXml.createTextNode(aAttrib['packaging']))
+
+		# Loop over all versions.
+		for (strVersion,bMatch) in aAttrib['versions'].iteritems():
+			tNode_Version = tNode_Target.appendChild(tXml.createElement('version'))
+			tNode_Version.setAttribute('match', str(bMatch))
+			tNode_Version.appendChild(tXml.createTextNode(strVersion))
+
+	# Write the file.
+	tFile = open(strFileName, 'wt')
+	tXml.writexml(tFile, indent='', addindent='\t', newl='\n', encoding='UTF-8')
+	tFile.close()
+
+
+def execute_scan(aArtifacts):
+	# Create the rest driver for httplib2.
+	tRestDriver = rest_driver_httplib2.RestDriver()
+
+	# Create the repository driver.
+	tRepository = repository_driver_nexus.RepositoryDriver(tRestDriver, 'http://nexus.netx01.hilscher.local/')
+
+	sizArtifacts = len(aArtifacts)
+	print 'Found %d artifacts.' % sizArtifacts
+
+	# Process all Target nodes.
+	sizArtifactCnt = 0
+	for tArtifact in aArtifacts.itervalues():
+		sizArtifactCnt += 1
+		print 'Processing artifact %d/%d (%d%%)' % (sizArtifactCnt, sizArtifacts, sizArtifactCnt*100/sizArtifacts)
 
 		# Get all revisions of the artifact.
-		atGAVersions = tRepository.getAllArtifactVersions(strGroupID, strArtifactID)
+		atGAVersions = tRepository.getAllArtifactVersions(tArtifact['gid'], tArtifact['aid'])
 
 		# Get all repository versions with the same SHA1 sum.
-		atSha1Versions = tRepository.findSha1Artifacts(strFile, strGroupID, strArtifactID)
-
-		# Add the revision information to the node.
-		for tVersion in atGAVersions:
-			tSubElem = SubElement(tNode, 'version')
-			tSubElem.text = str(tVersion)
-			bMatch = tVersion in atSha1Versions
-			tSubElem.set('match', str(bMatch))
-
+		atSha1Versions = tRepository.findSha1Artifacts(tArtifact['file'], tArtifact['gid'], tArtifact['aid'])
 
 		# Sanity test: all matching versions must also exist.
 		for tVersion in atSha1Versions:
 			if not tVersion in atGAVersions:
 				raise Exception('Artifact %s.%s: version %s does not exist!' % (strGroupID, strArtifactID, str(tVersion)))
 
+		# Add the revision information to the node.
+		aVersions = dict({})
+		for tVersion in atGAVersions:
+			bMatch = tVersion in atSha1Versions
+			aVersions[str(tVersion)] = bMatch
+
+		tArtifact['versions'] = aVersions
 
 
 def main(argv):
@@ -149,14 +218,17 @@ def main(argv):
 	print 'strOutputFileName = ' + str(aOptions.strOutputFileName)
 
 	# Read the artifact list.
-	tXml = ElementTree()
-	tXml.parse('targets/artifacts.xml')
+	aArtifacts = read_xml('targets/artifacts.xml')
 
 	if aOptions.bScan==True:
-		execute_scan(tXml)
+		execute_scan(aArtifacts)
 
+	if aOptions.aSelect!=None:
+		for strSelect in aOptions.aSelect:
+			print 'select = ' + strSelect
+	
 	# Write the new XML tree.
-	tXml.write(aOptions.strOutputFileName, encoding="UTF-8", xml_declaration=True, method="xml")
+	write_xml(aOptions.strOutputFileName, aArtifacts)
 
 
 
