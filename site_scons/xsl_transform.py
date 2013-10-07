4cac63d4d84d737ceb753e20aab35020d0b0dc0b
doc_bacardi <devnull@localhost>
2013-10-07 17:57:07 +0200

    Add scanner for XSLT files.

--- site_scons/xsl_transform.py
+++ site_scons/xsl_transform.py
@@ -21,9 +21,30 @@
 
 import xslt
 
+import re
+import os.path
+
 import SCons
 import SCons.Node.FS
 from SCons.Script import *
+
+
+
+xslt_include_re = re.compile(r'<xsl:include\s+href="(\S+)"\s*/>', re.M)
+
+def xslt_scan(node, env, path):
+	contents = node.get_text_contents()
+	astrIncludes = xslt_include_re.findall(contents)
+	
+	astrInc = []
+	for strInc in astrIncludes:
+		if os.path.isabs(strInc):
+			strIncPath = strInc
+		else:
+			strIncPath = os.path.abspath(os.path.join(os.path.dirname(node.get_path()), strInc))
+		astrInc.append(strIncPath)
+	return env.File(astrInc)
+
 
 
 def xslt_action(target, source, env):
@@ -67,4 +88,10 @@ def ApplyToEnv(env):
 	xslt_act = SCons.Action.Action(xslt_action, xslt_string)
 	xslt_bld = Builder(action=xslt_act, emitter=xslt_emitter)
 	env['BUILDERS']['XSLT'] = xslt_bld
+	
+	# Add the XSLT scanner.
+	xslt_scanner = Scanner(function = xslt_scan, skeys = ['.xsl', '.xslt'])
+	env.Append(SCANNERS = xslt_scanner)
+
+
 
