43ddaab71edd851caa031d8b5a8e2a3db77e6b07
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2016-08-02 16:00:04 +0200

    Add the hboot_snippet and pom_template builder.

---
+++ site_scons/pom_template.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+
+import codecs
+import os
+import string
+
+from SCons.Script import *
+
+
+def pom_template_action(target, source, env):
+    aSubst = dict({
+        'GROUP': str(env['POM_TEMPLATE_GROUP']),
+        'ARTIFACT': str(env['POM_TEMPLATE_ARTIFACT']),
+        'VERSION': str(env['POM_TEMPLATE_VERSION']),
+        'PACKAGING': str(env['POM_TEMPLATE_PACKAGING'])
+    })
+
+    # Read the template.
+    tFile = codecs.open(source[0].get_path(), 'r')
+    strTemplate = tFile.read()
+    tFile.close()
+
+    tTemplate = string.Template(strTemplate)
+    strPOM = tTemplate.substitute(aSubst)
+
+    # Write the POM to the target file.
+    tFile = codecs.open(target[0].get_path(), 'w', 'utf-8')
+    tFile.write(strPOM)
+    tFile.close()
+
+    return 0
+
+
+def pom_template_emitter(target, source, env):
+    # Make the target depend on the POM fields.
+    Depends(target, SCons.Node.Python.Value(PROJECT_VERSION))
+    Depends(target, SCons.Node.Python.Value(env['POM_TEMPLATE_GROUP']))
+    Depends(target, SCons.Node.Python.Value(env['POM_TEMPLATE_ARTIFACT']))
+    Depends(target, SCons.Node.Python.Value(env['POM_TEMPLATE_VERSION']))
+    Depends(target, SCons.Node.Python.Value(env['POM_TEMPLATE_PACKAGING']))
+
+    return target, source
+
+
+def pom_template_string(target, source, env):
+    return 'POMTemplate %s' % target[0].get_path()
+
+
+def ApplyToEnv(env):
+    #----------------------------------------------------------------------------
+    #
+    # Add POMTemplate builder.
+    #
+    env['POM_TEMPLATE_GROUP'] = ''
+    env['POM_TEMPLATE_ARTIFACT'] = ''
+    env['POM_TEMPLATE_VERSION'] = ''
+    env['POM_TEMPLATE_PACKAGING'] = ''
+    pom_template_act = SCons.Action.Action(pom_template_action, pom_template_string)
+    pom_template_bld = Builder(action=pom_template_act, emitter=pom_template_emitter, single_source=1)
+    env['BUILDERS']['POMTemplate'] = pom_template_bld
