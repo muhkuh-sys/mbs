1550ef9cc880e9cfe631f178ec34ad589a87b4d0
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2016-10-28 14:52:18 +0200

    Add a long form of the VCS version where a tag also gets a commit revision.

--- site_scons/version.py
+++ site_scons/version.py
@@ -35,8 +35,11 @@ def build_version_strings(env):
         # The default version is 'unknown'.
         strProjectVersionVcsSystem = 'unknown'
         strProjectVersionVcsVersion = 'unknown'
+        strProjectVersionVcsVersionLong = 'unknown'
         strProjectVersionVCS = 'unknown'
+        strProjectVersionVCSLong = 'unknown'
         strProjectVersionLastCommit = 'unknown'
+        strProjectVersionVCSURL = 'unknown'
 
         # Use the root folder to get the version. This is important for HG
         # and SVN>=1.7 .
@@ -52,7 +55,8 @@ def build_version_strings(env):
                         'describe',
                         '--abbrev=12',
                         '--always',
-                        '--dirty=+'
+                        '--dirty=+',
+                        '--long'
                     ])
                     strGitId = string.strip(strOutput)
                     tMatch = re.match('[0-9a-f]{12}\+?$', strGitId)
@@ -60,27 +64,42 @@ def build_version_strings(env):
                         # This is a repository with no tags.
                         # Use the raw SHA sum.
                         strProjectVersionVcsVersion = strGitId
+                        strProjectVersionVcsVersionLong = strGitId
                     else:
-                        tMatch = re.match('v(\d+\.\d+\.\d+)$', strGitId)
-                        if tMatch is not None:
-                            # This is a repository which is exactly on a tag.
-                            # Use the tag name.
-                            strProjectVersionVcsVersion = tMatch.group(1)
+                        tMatch = re.match(
+                            'v(\d+(\.\d+)*)-(\d+)-g([0-9a-f]{12}\+?)$',
+                            strGitId
+                        )
+                        if tMatch is None:
+                            # The description has an unknown format.
+                            strProjectVersionVcsVersion = strGitId
+                            strProjectVersionVcsVersionLong = strGitId
                         else:
-                            tMatch = re.match(
-                                'v\d+\.\d+\.\d+-\d+-g([0-9a-f]{12}\+?)$',
-                                strGitId
-                            )
-                            if tMatch is not None:
-                                # This is a repository with commits after
-                                # the last tag.
+                            ulRevsSinceTag = long(tMatch.group(3))
+                            if ulRevsSinceTag==0:
+                                # This is a repository which is exactly on a
+                                # tag. Use the tag name.
                                 strProjectVersionVcsVersion = tMatch.group(1)
+                                strProjectVersionVcsVersionLong = '%s-%s' % (tMatch.group(1), tMatch.group(4))
                             else:
-                                # The description has an unknown format.
-                                strProjectVersionVcsVersion = strGitId
+                                # This is a repository with commits after
+                                # the last tag. Use the checkin ID.
+                                strProjectVersionVcsVersion = tMatch.group(4)
+                                strProjectVersionVcsVersionLong = tMatch.group(4)
+
                     strProjectVersionVCS = strProjectVersionVcsSystem + strProjectVersionVcsVersion
+                    strProjectVersionVCSLong = strProjectVersionVcsSystem + strProjectVersionVcsVersionLong
+
+                    strOutput = subprocess.check_output([
+                        env['GIT'],
+                        'remote',
+                        'get-url',
+                        'origin'
+                    ])
+                    strProjectVersionVCSURL = string.strip(strOutput)
                 except:
                     pass
+
         elif os.path.exists(os.path.join(strSconsRoot, '.hg')):
             if env['MERCURIAL']:
                 strProjectVersionVcsSystem = 'HG'
@@ -134,14 +153,22 @@ def build_version_strings(env):
 
         # Add the version to the environment.
         env['PROJECT_VERSION_VCS'] = strProjectVersionVCS
+        env['PROJECT_VERSION_VCS_LONG'] = strProjectVersionVCSLong
         env['PROJECT_VERSION_LAST_COMMIT'] = strProjectVersionLastCommit
         env['PROJECT_VERSION_VCS_SYSTEM'] = strProjectVersionVcsSystem
         env['PROJECT_VERSION_VCS_VERSION'] = strProjectVersionVcsVersion
+        env['PROJECT_VERSION_VCS_VERSION_LONG'] = strProjectVersionVcsVersionLong
+        env['PROJECT_VERSION_VCS_URL'] = strProjectVersionVCSURL
 
 
 def get_project_version_vcs(env):
     build_version_strings(env)
     return env['PROJECT_VERSION_VCS']
+
+
+def get_project_version_vcs_long(env):
+    build_version_strings(env)
+    return env['PROJECT_VERSION_VCS_LONG']
 
 
 def get_project_version_last_commit(env):
@@ -157,6 +184,16 @@ def get_project_version_vcs_system(env):
 def get_project_version_vcs_version(env):
     build_version_strings(env)
     return env['PROJECT_VERSION_VCS_VERSION']
+
+
+def get_project_version_vcs_version_long(env):
+    build_version_strings(env)
+    return env['PROJECT_VERSION_VCS_VERSION_LONG']
+
+
+def get_project_version_vcs_url(env):
+    build_version_strings(env)
+    return env['PROJECT_VERSION_VCS_URL']
 
 
 def version_action(target, source, env):
@@ -202,9 +239,12 @@ def version_emitter(target, source, env):
     # Make the target depend on the project version and the VCS ID.
     env.Depends(target, SCons.Node.Python.Value(PROJECT_VERSION))
     env.Depends(target, SCons.Node.Python.Value(env['PROJECT_VERSION_VCS']))
+    env.Depends(target, SCons.Node.Python.Value(env['PROJECT_VERSION_VCS_LONG']))
     env.Depends(target, SCons.Node.Python.Value(env['PROJECT_VERSION_LAST_COMMIT']))
     env.Depends(target, SCons.Node.Python.Value(env['PROJECT_VERSION_VCS_SYSTEM']))
     env.Depends(target, SCons.Node.Python.Value(env['PROJECT_VERSION_VCS_VERSION']))
+    env.Depends(target, SCons.Node.Python.Value(env['PROJECT_VERSION_VCS_VERSION_LONG']))
+    env.Depends(target, SCons.Node.Python.Value(env['PROJECT_VERSION_VCS_URL']))
 
     return target, source
 
@@ -227,6 +267,9 @@ def ApplyToEnv(env):
     env['BUILDERS']['Version'] = version_bld
 
     env.AddMethod(get_project_version_vcs, "Version_GetVcsId")
+    env.AddMethod(get_project_version_vcs_long, "Version_GetVcsIdLong")
     env.AddMethod(get_project_version_last_commit, "Version_GetLastCommit")
     env.AddMethod(get_project_version_vcs_system, "Version_GetVcsSystem")
     env.AddMethod(get_project_version_vcs_version, "Version_GetVcsVersion")
+    env.AddMethod(get_project_version_vcs_version_long, "Version_GetVcsVersionLong")
+    env.AddMethod(get_project_version_vcs_url, 'Version_GetVcsUrl')
