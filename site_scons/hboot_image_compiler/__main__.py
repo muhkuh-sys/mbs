8bef547eba1bd4e69b8da2f029d4a192165f2317
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2016-12-14 17:33:54 +0100

    Add the netX90 MPW APP target to the command line interface.

--- site_scons/hboot_image_compiler/__main__.py
+++ site_scons/hboot_image_compiler/__main__.py
@@ -10,7 +10,7 @@ tParser = argparse.ArgumentParser(usage='usage: hboot_image [options]')
 tParser.add_argument('-n', '--netx-type',
                      dest='strNetxType',
                      required=True,
-                     choices=['NETX56', 'NETX90_MPW', 'NETX4000_RELAXED'],
+                     choices=['NETX56', 'NETX90_MPW', 'NETX90_MPW', 'NETX4000_RELAXED'],
                      metavar='NETX',
                      help='Build the image for netx type NETX.')
 tParser.add_argument('-c', '--objcopy',
@@ -73,6 +73,7 @@ tArgs = tParser.parse_args()
 atDefaultPatchTables = {
     'NETX56': 'hboot_netx56_patch_table.xml',
     'NETX90_MPW': 'hboot_netx90_mpw_patch_table.xml',
+    'NETX90_MPW_APP': 'hboot_netx90_mpw_app_patch_table.xml',
     'NETX4000_RELAXED': 'hboot_netx4000_relaxed_patch_table.xml'
 }
 if tArgs.strPatchTablePath is None:
