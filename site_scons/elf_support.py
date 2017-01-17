ba8ef0387b55862b77defb252ae435b34e961e69
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2017-01-17 13:00:57 +0100

    Fix flake8 warnings.

--- site_scons/elf_support.py
+++ site_scons/elf_support.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#-------------------------------------------------------------------------#
+# ----------------------------------------------------------------------- #
 #   Copyright (C) 2010 by Christoph Thelen                                #
 #   doc_bacardi@users.sourceforge.net                                     #
 #                                                                         #
@@ -17,14 +17,12 @@
 #   along with this program; if not, write to the                         #
 #   Free Software Foundation, Inc.,                                       #
 #   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             #
-#-------------------------------------------------------------------------#
+# ----------------------------------------------------------------------- #
 
 
-import itertools
 import os
 import re
 import subprocess
-#import xml.etree.ElementTree
 
 # NOTE: this is only for debug.
 import datetime
@@ -41,17 +39,16 @@ def get_segment_table(env, strFileName, astrSegmentsToConsider=None):
             uiAlign = eval(match_obj.group(7))
             astrFlags = match_obj.group(8).split(', ')
             atSegments.append(dict({
-                'idx':        long(match_obj.group(1)),
-                'name' :    strName,
-                'size' :    long(match_obj.group(3),16),
-                'vma' :        long(match_obj.group(4),16),
-                'lma' :        long(match_obj.group(5),16),
-                'file_off' :    long(match_obj.group(6),16),
-                'align' :    uiAlign,
-                'flags' :    astrFlags
+                'idx':      int(match_obj.group(1)),
+                'name':     strName,
+                'size':     int(match_obj.group(3), 16),
+                'vma':      int(match_obj.group(4), 16),
+                'lma':      int(match_obj.group(5), 16),
+                'file_off': int(match_obj.group(6), 16),
+                'align':    uiAlign,
+                'flags':    astrFlags
             }))
     return atSegments
-
 
 
 def get_symbol_table(env, strFileName):
@@ -65,13 +62,12 @@ def get_symbol_table(env, strFileName):
 
     for strLine in strOutput.split(os.linesep):
         tObj = reSymbol.match(strLine)
-        if not tObj is None:
-            ulValue = long(tObj.group(1), 16)
+        if tObj is not None:
+            ulValue = int(tObj.group(1), 16)
             strName = tObj.group(2)
             atSymbols[strName] = ulValue
 
     return atSymbols
-
 
 
 def get_debug_structure(env, strFileName):
@@ -82,8 +78,6 @@ def get_debug_structure(env, strFileName):
     time_start = datetime.datetime.now()
 
     # Add all information to an XML file.
-    #tRoot = xml.etree.ElementTree.Element('DebugInfo')
-    #tXml = xml.etree.ElementTree.ElementTree(tRoot)
     atDebugInfo = dict({'name': None, 'abbrev': None, 'children': [], 'attributes': dict({})})
 
     # Prepare the regular expressions for the elements.
@@ -100,14 +94,14 @@ def get_debug_structure(env, strFileName):
     for strLine in strOutput.split(os.linesep):
         # Is this a new element?
         tObj = reElement.match(strLine)
-        if not tObj is None:
+        if tObj is not None:
             uiNodeLevel = int(tObj.group(1))
             ulNodeId = int(tObj.group(2), 16)
             ulAbbrev = int(tObj.group(3))
             strName = tObj.group(4)
 
             # Get the parent node.
-            if uiNodeLevel<0 or uiNodeLevel>=len(atParentNode):
+            if (uiNodeLevel < 0) or (uiNodeLevel >= len(atParentNode)):
                 raise Exception('Invalid node level: %d', uiNodeLevel)
             tParentNode = atParentNode[uiNodeLevel]
             if tParentNode is None:
@@ -117,9 +111,6 @@ def get_debug_structure(env, strFileName):
             atParentNode = atParentNode[0:uiNodeLevel+1]
 
             # Create the new element.
-            #tNode = xml.etree.ElementTree.SubElement(tParentNode, strName)
-            #tNode.set('id', str(ulNodeId))
-            #tNode.set('abbrev', str(ulAbbrev))
             atNodeData = dict({'name': strName, 'id': ulNodeId, 'attributes': dict({'abbrev': ulAbbrev}), 'children': []})
             tParentNode['children'].append(atNodeData)
 
@@ -127,7 +118,7 @@ def get_debug_structure(env, strFileName):
             atParentNode.append(atNodeData)
         else:
             tObj = reAttribute_Link.match(strLine)
-            if not tObj is None:
+            if tObj is not None:
                 ulNodeId = int(tObj.group(1), 16)
                 strName = tObj.group(2)
                 ulValue = int(tObj.group(3), 16)
@@ -138,7 +129,7 @@ def get_debug_structure(env, strFileName):
                 if tObj is None:
                     tObj = reAttribute.match(strLine)
 
-                if not tObj is None:
+                if tObj is not None:
                     ulNodeId = int(tObj.group(1), 16)
                     strName = tObj.group(2)
                     strValue = tObj.group(3).strip()
@@ -146,7 +137,7 @@ def get_debug_structure(env, strFileName):
                     tNode['attributes'][strName] = strValue
 
     time_end = datetime.datetime.now()
-    print 'Time used:', str(time_end-time_start)
+    print('Time used:', str(time_end-time_start))
 
 #    # Write the XML tree to a test file.
 #    astrXml = xml.etree.ElementTree.tostringlist(tXml.getroot(), encoding='UTF-8', method="xml")
@@ -157,25 +148,24 @@ def get_debug_structure(env, strFileName):
     return atDebugInfo
 
 
-
 s_reLocation = re.compile('\d+ byte block: \d+ ([0-9a-f]+)')
+
 
 def __iter_debug_info(tNode, atSymbols):
     strName = tNode['name']
     tAttr = tNode['attributes']
 
     # Is this an enumerator type?
-    if strName=='enumerator':
-        if not 'const_value' in tAttr:
+    if strName == 'enumerator':
+        if 'const_value' not in tAttr:
             raise Exception('Missing const_value')
-        if not 'name' in tAttr:
+        if 'name' not in tAttr:
             raise Exception('Missing name')
-
 
         ulValue = int(tAttr['const_value'])
         strName = tAttr['name']
         atSymbols[strName] = ulValue
-    elif strName=='structure_type':
+    elif strName == 'structure_type':
         if 'name' in tAttr:
             strStructureName = tAttr['name']
             # Generate a symbol with the size of the structure.
@@ -183,13 +173,13 @@ def __iter_debug_info(tNode, atSymbols):
             atSymbols[strMemberName] = tAttr['byte_size']
             # Generate symbols for the offset of each member.
             for tMember in tNode['children']:
-                if tMember['name']=='member':
+                if tMember['name'] == 'member':
                     tMemberAttr = tMember['attributes']
                     strLoc = tMemberAttr['data_member_location']
                     strName = tMemberAttr['name']
-                    if (not strLoc is None) and (not strName is None):
+                    if (strLoc is not None) and (strName is not None):
                         tObj = s_reLocation.match(strLoc)
-                        if not tObj is None:
+                        if tObj is not None:
                             strMemberName = 'OFFSETOF_' + strStructureName + '_' + strName
                             ulOffset = int(tObj.group(1), 16)
                             atSymbols[strMemberName] = ulOffset
@@ -198,13 +188,11 @@ def __iter_debug_info(tNode, atSymbols):
             __iter_debug_info(tChild, atSymbols)
 
 
-
 def get_debug_symbols(env, strFileName):
     atDebugInfo = get_debug_structure(env, strFileName)
     atAllSymbols = dict({})
     __iter_debug_info(atDebugInfo, atAllSymbols)
     return atAllSymbols
-
 
 
 def get_macro_definitions(env, strFileName):
@@ -228,24 +216,23 @@ def get_macro_definitions(env, strFileName):
         # Is this a new element?
         for reMacro in areMacro:
             tObj = reMacro.match(strLine)
-            if not tObj is None:
+            if tObj is not None:
                 strName = tObj.group(1)
                 strValue = tObj.group(2)
 
                 # Does the macro already exist?
                 if strName in atMergedMacros:
                     # Yes, it exists already. Is the value the same?
-                    if not(atMergedMacros[strName] is None) and (atMergedMacros[strName]!=strValue):
+                    if not(atMergedMacros[strName] is None) and (atMergedMacros[strName] != strValue):
                         # The macro exists more than one time with different values. Now that's a problem.
                         atMergedMacros[strName] = None
                 else:
                     atMergedMacros[strName] = strValue
 
     time_end = datetime.datetime.now()
-    print 'Time used:', str(time_end-time_start)
+    print('Time used:', str(time_end-time_start))
 
     return atMergedMacros
-
 
 
 def get_load_address(atSegments):
@@ -255,14 +242,13 @@ def get_load_address(atSegments):
     # Loop over all segments.
     for tSegment in atSegments:
         # Get the segment with the lowest 'lma' entry which has also the flags 'CONTENTS', 'ALLOC' and 'LOAD'.
-        if (tSegment['lma']<ulLowestLma) and ('CONTENTS' in tSegment['flags']) and ('ALLOC' in tSegment['flags']) and ('LOAD' in tSegment['flags']):
+        if (tSegment['lma'] < ulLowestLma) and ('CONTENTS' in tSegment['flags']) and ('ALLOC' in tSegment['flags']) and ('LOAD' in tSegment['flags']):
             ulLowestLma = tSegment['lma']
 
-    if ulLowestLma==0x100000000:
+    if ulLowestLma == 0x100000000:
         raise Exception("failed to extract load address!")
 
     return ulLowestLma
-
 
 
 def get_estimated_bin_size(atSegments):
@@ -274,11 +260,10 @@ def get_estimated_bin_size(atSegments):
         # Get the segment with the biggest offset to ulLoadAddress which has also the flags 'CONTENTS', 'ALLOC' and 'LOAD'.
         if ('CONTENTS' in tSegment['flags']) and ('ALLOC' in tSegment['flags']) and ('LOAD' in tSegment['flags']):
             ulOffset = tSegment['lma'] + tSegment['size'] - ulLoadAddress
-            if ulOffset>ulBiggestOffset:
+            if ulOffset > ulBiggestOffset:
                 ulBiggestOffset = ulOffset
 
     return ulBiggestOffset
-
 
 
 def get_exec_address(env, strElfFileName):
@@ -288,10 +273,9 @@ def get_exec_address(env, strElfFileName):
     strOutput = proc.communicate()[0]
     match_obj = re.search('Entry point address:\s+0x([0-9a-fA-F]+)', strOutput)
     if not match_obj:
-        print 'Failed to extract start address.'
-        print 'Command:', aCmd
-        print 'Output:', strOutput
+        print('Failed to extract start address.')
+        print('Command:', aCmd)
+        print('Output:', strOutput)
         raise Exception('Failed to extract start address.')
 
-    return long(match_obj.group(1), 16)
-
+    return int(match_obj.group(1), 16)
