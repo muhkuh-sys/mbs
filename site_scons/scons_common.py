a8ef0b97335a1df817ee6ecf99affe1ab12ec06c
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2017-08-10 10:33:53 +0200

    Fix all flake8 warnings.

--- site_scons/scons_common.py
+++ site_scons/scons_common.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#-------------------------------------------------------------------------#
+# ----------------------------------------------------------------------- #
 #   Copyright (C) 2010 by Christoph Thelen                                #
 #   doc_bacardi@users.sourceforge.net                                     #
 #                                                                         #
@@ -17,16 +17,15 @@
 #   along with this program; if not, write to the                         #
 #   Free Software Foundation, Inc.,                                       #
 #   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             #
-#-------------------------------------------------------------------------#
+# ----------------------------------------------------------------------- #
 
 
+import atexit
 import imp
 import os
 import re
 import subprocess
-import sys
-
-from SCons.Script import *
+import SCons
 
 
 # Import all local modules.
@@ -54,18 +53,20 @@ import version
 import xsl_transform
 
 
-
-#----------------------------------------------------------------------------
+# ---------------------------------------------------------------------------
 #
 # Accept 'clean' target like make as an alternative to '-c'. This makes it
 # much easier to work with an IDE like KDevelop.
 #
-if 'clean' in COMMAND_LINE_TARGETS:
-	Alias('clean', SCons.Node.FS.get_default_fs().SConstruct_dir.abspath)
-	SetOption('clean', 1)
+if 'clean' in SCons.Script.COMMAND_LINE_TARGETS:
+    SCons.Script.Alias(
+        'clean',
+        SCons.Node.FS.get_default_fs().SConstruct_dir.abspath
+    )
+    SCons.Script.Main.SetOption('clean', 1)
 
 
-#----------------------------------------------------------------------------
+# ---------------------------------------------------------------------------
 #
 # Display the complete command line if any command failed.
 #
@@ -74,171 +75,189 @@ if 'clean' in COMMAND_LINE_TARGETS:
 #       after the stack dump. How can I detect this here?
 #
 def display_build_status():
-	from SCons.Script import GetBuildFailures
-	bf_all = GetBuildFailures()
-	if bf_all:
-		# iterate over all failures and print the command
-		for bf in bf_all:
-			if bf and bf.node and bf.command:
-				print 'Failed command for ' + str(bf.node) + ":\n" + ' '.join(bf.command)
-		print "!!!!!!!!!!!!!!!"
-		print "!!! FAILED !!!!"
-		print "!!!!!!!!!!!!!!!"
+    from SCons.Script import GetBuildFailures
+    bf_all = GetBuildFailures()
+    if bf_all:
+        # iterate over all failures and print the command
+        for bf in bf_all:
+            if bf and bf.node and bf.command:
+                print('Failed command for %s:\n' % str(bf.node))
+                print(' '.join(bf.command))
+        print("!!!!!!!!!!!!!!!")
+        print("!!! FAILED !!!!")
+        print("!!!!!!!!!!!!!!!")
 
-import atexit
 atexit.register(display_build_status)
 
 
-
 def find_first_tool(strToolPattern):
-	global TOOLS
-	strToolName = None
+    strToolName = None
 
-	tPattern = re.compile(strToolPattern)
-	for strKey in TOOLS.iterkeys():
-		tMatch = re.search(tPattern, strKey)
-		if not tMatch is None:
-			strToolName = strKey
-			break
+    tPattern = re.compile(strToolPattern)
+    for strKey in SCons.Script.TOOLS.iterkeys():
+        tMatch = re.search(tPattern, strKey)
+        if tMatch is not None:
+            strToolName = strKey
+            break
 
-	return strToolName
-
+    return strToolName
 
 
 def get_tool(env, strToolName):
-	global TOOLS
+    tMod = None
+    try:
+        strPath = SCons.Script.TOOLS[strToolName]
+        strModulName = strToolName.replace('-', '_').replace('.', '_')
+        fp, pathname, description = imp.find_module(strModulName, [strPath])
+        try:
+            tMod = imp.load_module(strModulName, fp, pathname, description)
+        finally:
+            # Since we may exit via an exception, close fp explicitly.
+            if fp:
+                fp.close()
+    except KeyError:
+        pass
 
-	tMod = None
-	try:
-		strPath = TOOLS[strToolName]
-		strModulName = strToolName.replace('-','_').replace('.','_')
-		fp,pathname,description = imp.find_module(strModulName, [strPath])
-		try:
-			tMod = imp.load_module(strModulName, fp, pathname, description)
-		finally:
-			# Since we may exit via an exception, close fp explicitly.
-			if fp:
-				fp.close()
-	except KeyError:
-		pass
+    if tMod is None:
+        raise Exception(
+            strToolName,
+            'The requested tool is not part of the configuration. '
+            'Add it to setup.xml and rerun mbs.')
 
-	if tMod==None:
-		raise Exception(strToolName, 'The requested tool is not part of the configuration. Add it to setup.xml and rerun mbs.')
-
-	return tMod
-
+    return tMod
 
 
 def CreateEnvironment(env, astrToolPatterns=None):
-	# Create the new environment.
-	tEnvNew = Environment()
-	tEnvNew.Decider('MD5')
+    # Create the new environment.
+    tEnvNew = SCons.Environment.Environment()
+    tEnvNew.Decider('MD5')
 
-	if not astrToolPatterns is None:
-		for strToolPattern in astrToolPatterns:
-			strId = find_first_tool(strToolPattern)
-			if strId is None:
-				raise Exception('Nothing found searching for a tool matching the pattern "%s".' % strToolPattern)
-			add_tool_to_environment(tEnvNew, strId)
+    if astrToolPatterns is not None:
+        for strToolPattern in astrToolPatterns:
+            strId = find_first_tool(strToolPattern)
+            if strId is None:
+                raise Exception(
+                    'Nothing found searching for a tool matching the '
+                    'pattern "%s".' % strToolPattern
+                )
+            add_tool_to_environment(tEnvNew, strId)
 
-	archive.ApplyToEnv(tEnvNew)
-	artifact.ApplyToEnv(tEnvNew)
-	artifact_version.ApplyToEnv(tEnvNew)
-	bootblock.ApplyToEnv(tEnvNew)
-	build_properties.ApplyToEnv(tEnvNew)
-	data_array.ApplyToEnv(tEnvNew)
-	diff.ApplyToEnv(tEnvNew)
-	filter.ApplyToEnv(tEnvNew)
-	flex_zip.ApplyToEnv(tEnvNew)
-	gcc_symbol_template.ApplyToEnv(tEnvNew)
-	gen_random_seq.ApplyToEnv(tEnvNew)
-	hash.ApplyToEnv(tEnvNew)
-	hboot_image.ApplyToEnv(tEnvNew)
-	hboot_snippet.ApplyToEnv(tEnvNew)
-	hexdump.ApplyToEnv(tEnvNew)
-	iflash_image.ApplyToEnv(tEnvNew)
-	objimport.ApplyToEnv(tEnvNew)
-	pom_template.ApplyToEnv(tEnvNew)
-	ApplyToEnv(tEnvNew)
-	svnversion.ApplyToEnv(tEnvNew)
-	uuencode.ApplyToEnv(tEnvNew)
-	version.ApplyToEnv(tEnvNew)
-	xsl_transform.ApplyToEnv(tEnvNew)
+    archive.ApplyToEnv(tEnvNew)
+    artifact.ApplyToEnv(tEnvNew)
+    artifact_version.ApplyToEnv(tEnvNew)
+    bootblock.ApplyToEnv(tEnvNew)
+    build_properties.ApplyToEnv(tEnvNew)
+    data_array.ApplyToEnv(tEnvNew)
+    diff.ApplyToEnv(tEnvNew)
+    filter.ApplyToEnv(tEnvNew)
+    flex_zip.ApplyToEnv(tEnvNew)
+    gcc_symbol_template.ApplyToEnv(tEnvNew)
+    gen_random_seq.ApplyToEnv(tEnvNew)
+    hash.ApplyToEnv(tEnvNew)
+    hboot_image.ApplyToEnv(tEnvNew)
+    hboot_snippet.ApplyToEnv(tEnvNew)
+    hexdump.ApplyToEnv(tEnvNew)
+    iflash_image.ApplyToEnv(tEnvNew)
+    objimport.ApplyToEnv(tEnvNew)
+    pom_template.ApplyToEnv(tEnvNew)
+    ApplyToEnv(tEnvNew)
+    svnversion.ApplyToEnv(tEnvNew)
+    uuencode.ApplyToEnv(tEnvNew)
+    version.ApplyToEnv(tEnvNew)
+    xsl_transform.ApplyToEnv(tEnvNew)
 
-	# Add the reference to the list of environments.
-	if env is not None:
-		tEnvNew.Replace(MBS_ENVIRONMENT_LIST = env['MBS_ENVIRONMENT_LIST'])
+    # Add the reference to the list of environments.
+    if env is not None:
+        tEnvNew.Replace(MBS_ENVIRONMENT_LIST=env['MBS_ENVIRONMENT_LIST'])
 
-	return tEnvNew
-
+    return tEnvNew
 
 
 def add_tool_to_environment(env, strToolIdAndVersion):
-	tTool = get_tool(env, strToolIdAndVersion)
-	tTool.ApplyToEnv(env)
-
+    tTool = get_tool(env, strToolIdAndVersion)
+    tTool.ApplyToEnv(env)
 
 
 def set_build_path(env, build_path, source_path, sources):
-	# Convert the sources to a list.
-	if isinstance(sources, str):
-		sources = Split(sources)
-	env.VariantDir(build_path, source_path, duplicate=0)
-	return [src.replace(source_path, build_path) for src in sources]
+    # Convert the sources to a list.
+    if isinstance(sources, str):
+        sources = SCons.Script.Split(sources)
+    env.VariantDir(build_path, source_path, duplicate=0)
+    return [src.replace(source_path, build_path) for src in sources]
 
 
-def create_compiler_environment(env, strAsicTyp, aAttributesCommon, linker_attributes=None, name=None):
-	# Find the library paths for gcc and newlib.
+def create_compiler_environment(
+    env,
+    strAsicTyp,
+    aAttributesCommon,
+    linker_attributes=None,
+    name=None
+):
+    # Find the library paths for gcc and newlib.
 
-	# Use the common attributes both for the detect and the linker phase if no special linker attributes were specified.
-	if linker_attributes is None:
-		linker_attributes = aAttributesCommon
+    # Use the common attributes both for the detect and the linker phase if no
+    # special linker attributes were specified.
+    if linker_attributes is None:
+        linker_attributes = aAttributesCommon
 
-	# Use the ASIC typ as the default name.
-	if name is None:
-		name = strAsicTyp
+    # Use the ASIC typ as the default name.
+    if name is None:
+        name = strAsicTyp
 
-	# Prepend an 'm' to each attribute and create a set from this list.
-	aMAttributesCommon = set(['m'+strAttr for strAttr in aAttributesCommon])
+    # Prepend an 'm' to each attribute and create a set from this list.
+    aMAttributesCommon = set(['m'+strAttr for strAttr in aAttributesCommon])
 
-	# Prepend an '-m' to each attribute.
-	aOptAttributesCommon = ['-m'+strAttr for strAttr in aAttributesCommon]
-	aOptAttributesLinker = ['-m'+strAttr for strAttr in linker_attributes]
+    # Prepend an '-m' to each attribute.
+    aOptAttributesCommon = ['-m'+strAttr for strAttr in aAttributesCommon]
+    aOptAttributesLinker = ['-m'+strAttr for strAttr in linker_attributes]
 
-	# Get the mapping for multiple library search directories.
-	strMultilibPath = None
-	aCmd = [env['CC']]
-	aCmd.extend(aOptAttributesCommon)
-	aCmd.append('-print-multi-lib')
-	proc = subprocess.Popen(aCmd, stdout=subprocess.PIPE)
-	strOutput = proc.communicate()[0]
-	for match_obj in re.finditer('^([^;]+);@?([^\r\n\t ]+)', strOutput, re.MULTILINE):
-		strPath = match_obj.group(1)
-		aAttr = set(match_obj.group(2).split('@'))
-		if aAttr==aMAttributesCommon:
-			strMultilibPath = strPath
-			break
+    # Get the mapping for multiple library search directories.
+    strMultilibPath = None
+    aCmd = [env['CC']]
+    aCmd.extend(aOptAttributesCommon)
+    aCmd.append('-print-multi-lib')
+    proc = subprocess.Popen(aCmd, stdout=subprocess.PIPE)
+    strOutput = proc.communicate()[0]
+    for match_obj in re.finditer(
+        '^([^;]+);@?([^\r\n\t ]+)',
+        strOutput,
+        re.MULTILINE
+    ):
+        strPath = match_obj.group(1)
+        aAttr = set(match_obj.group(2).split('@'))
+        if aAttr == aMAttributesCommon:
+            strMultilibPath = strPath
+            break
 
-	if strMultilibPath==None:
-		raise Exception('Could not find multilib configuration for attributes %s' % (','.join(aAttributesCommon)))
+    if strMultilibPath is None:
+        raise Exception(
+            'Could not find multilib configuration for attributes %s' %
+            (','.join(aAttributesCommon))
+        )
 
-	strGccLibPath = os.path.join(env['GCC_LIBRARY_DIR_COMPILER'], strMultilibPath)
-	strNewlibPath = os.path.join(env['GCC_LIBRARY_DIR_ARCHITECTURE'], strMultilibPath)
+    strGccLibPath = os.path.join(
+        env['GCC_LIBRARY_DIR_COMPILER'],
+        strMultilibPath
+    )
+    strNewlibPath = os.path.join(
+        env['GCC_LIBRARY_DIR_ARCHITECTURE'],
+        strMultilibPath
+    )
 
-	env_new = env.Clone()
-	env_new.Append(CCFLAGS = aOptAttributesLinker)
-	env_new.Replace(LIBPATH = [strGccLibPath, strNewlibPath])
-	env_new.Append(CPPDEFINES = [['ASIC_TYP', 'ASIC_TYP_%s' % strAsicTyp]])
-	env_new.Replace(ASIC_TYP = '%s' % strAsicTyp)
+    env_new = env.Clone()
+    env_new.Append(CCFLAGS=aOptAttributesLinker)
+    env_new.Replace(LIBPATH=[strGccLibPath, strNewlibPath])
+    env_new.Append(CPPDEFINES=[['ASIC_TYP', 'ASIC_TYP_%s' % strAsicTyp]])
+    env_new.Replace(ASIC_TYP='%s' % strAsicTyp)
 
-	# Add the new environment to the list.
-	setattr(env['MBS_ENVIRONMENT_LIST'], name, env_new)
+    # Add the new environment to the list.
+    setattr(env['MBS_ENVIRONMENT_LIST'], name, env_new)
 
-	return env_new
+    return env_new
 
 
 def ApplyToEnv(env):
-	env.AddMethod(set_build_path, 'SetBuildPath')
-	env.AddMethod(create_compiler_environment, 'CreateCompilerEnv')
-	env.AddMethod(get_tool, 'GetTool')
-	env.AddMethod(CreateEnvironment, "CreateEnvironment")
+    env.AddMethod(set_build_path, 'SetBuildPath')
+    env.AddMethod(create_compiler_environment, 'CreateCompilerEnv')
+    env.AddMethod(get_tool, 'GetTool')
+    env.AddMethod(CreateEnvironment, "CreateEnvironment")
