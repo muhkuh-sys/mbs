fac9fc2204d0df15f2858319b648a55f49aa1bd8
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2019-07-08 11:09:22 +0200

    Use the OS bit size instead of the CPU bit size if available.

--- mbs/install.py
+++ mbs/install.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#-------------------------------------------------------------------------#
+# ----------------------------------------------------------------------- #
 #   Copyright (C) 2010 by Christoph Thelen                                #
 #   doc_bacardi@users.sourceforge.net                                     #
 #                                                                         #
@@ -17,17 +17,19 @@
 #   along with this program; if not, write to the                         #
 #   Free Software Foundation, Inc.,                                       #
 #   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             #
-#-------------------------------------------------------------------------#
+# ----------------------------------------------------------------------- #
 
 import hashlib
 import os
 import os.path
-import platform
 import re
 import string
 import tarfile
 
 import download
+
+from platform_detect import PlatformDetect
+
 
 #
 # Check the Sha1 sum for a file.
@@ -38,215 +40,254 @@ import download
 # Returns 'True' on success and 'False' on error.
 #
 def check_sha1_sum(strSha1File, strBinFile):
-	bResult = False
-	strRemoteHash = None
-	
-	tRegObj = re.compile('([0-9a-fA-F]+)')
-	fInput = open(strSha1File, 'rt')
-	for strLine in fInput:
-		tMatchObj = tRegObj.match(strLine)
-		if tMatchObj:
-			# Get the hash.
-			strRemoteHash = tMatchObj.group(1)
-			break
-	fInput.close()
-	
-	
-	if strRemoteHash:
-		tHashObj = hashlib.sha1()
-	
-		fInput = open(strBinFile, 'rb')
-		while 1:
-			strChunk = fInput.read(8192)
-			if len(strChunk)==0:
-				break
-			tHashObj.update(strChunk)
-		fInput.close()
-	
-		strLocalHash = tHashObj.hexdigest()
-		if strRemoteHash==strLocalHash:
-			bResult = True
-	
-	return bResult
+    bResult = False
+    strRemoteHash = None
 
+    tRegObj = re.compile('([0-9a-fA-F]+)')
+    fInput = open(strSha1File, 'rt')
+    for strLine in fInput:
+        tMatchObj = tRegObj.match(strLine)
+        if tMatchObj:
+            # Get the hash.
+            strRemoteHash = tMatchObj.group(1)
+            break
+    fInput.close()
+
+    if strRemoteHash:
+        tHashObj = hashlib.sha1()
+
+        fInput = open(strBinFile, 'rb')
+        while 1:
+            strChunk = fInput.read(8192)
+            if len(strChunk) == 0:
+                break
+            tHashObj.update(strChunk)
+        fInput.close()
+
+        strLocalHash = tHashObj.hexdigest()
+        if strRemoteHash == strLocalHash:
+            bResult = True
+
+    return bResult
 
 
 def getToolAttributes(aCfg, aTool, strPackageMachine):
-	aAttr = dict({})
+    aAttr = dict({})
 
-	# Construct the path for the marker.
-	aAttr['LocalMarkerFolder'] = aCfg['marker_path']
-	
-	# Construct the path to the repository folder.
-	aAttr['LocalRepositoryPath'] = aCfg['repository_path']
-	
-	# Construct the path to the depack folder.
-	aAttr['PacketDepackPath'] = os.path.join(aCfg['depack_path'], aTool['group'], aTool['name'])
-	
-	# Construct the package name.
-	strPackageName = '%s-%s.%s'%(strPackageMachine,aTool['version'],aTool['typ'])
-	aAttr['PackageName'] = strPackageName
-	aAttr['Sha1Name'] = strPackageName + '.sha1'
-	
-	aPathElements = aTool['group'].split('.')
-	aPathElements.append(strPackageMachine)
-	aPathElements.append(aTool['version'])
-	aAttr['PathElements'] = aPathElements
-	
-	aAttr['LocalMarkerPath'] = os.path.join(aCfg['marker_path'], '%s-%s-%s-%s.marker'%(aTool['group'],aTool['name'],aTool['typ'],aTool['version']))
-	
-	# Construct the path in the repository.
-	aAttr['LocalPackageFolder'] = os.path.join(aCfg['repository_path'], *aPathElements)
-	aAttr['LocalPackagePath'] = os.path.join(aAttr['LocalPackageFolder'], strPackageName)
-	aAttr['LocalSha1Path'] = aAttr['LocalPackagePath'] + '.sha1'
+    # Construct the path for the marker.
+    aAttr['LocalMarkerFolder'] = aCfg['marker_path']
 
-	return aAttr
+    # Construct the path to the repository folder.
+    aAttr['LocalRepositoryPath'] = aCfg['repository_path']
 
+    # Construct the path to the depack folder.
+    aAttr['PacketDepackPath'] = os.path.join(
+        aCfg['depack_path'],
+        aTool['group'],
+        aTool['name']
+    )
+
+    # Construct the package name.
+    strPackageName = '%s-%s.%s' % (
+        strPackageMachine,
+        aTool['version'],
+        aTool['typ']
+    )
+    aAttr['PackageName'] = strPackageName
+    aAttr['Sha1Name'] = strPackageName + '.sha1'
+
+    aPathElements = aTool['group'].split('.')
+    aPathElements.append(strPackageMachine)
+    aPathElements.append(aTool['version'])
+    aAttr['PathElements'] = aPathElements
+
+    aAttr['LocalMarkerPath'] = os.path.join(
+        aCfg['marker_path'], '%s-%s-%s-%s.marker' % (
+            aTool['group'],
+            aTool['name'],
+            aTool['typ'],
+            aTool['version']
+        )
+    )
+
+    # Construct the path in the repository.
+    aAttr['LocalPackageFolder'] = os.path.join(
+        aCfg['repository_path'],
+        *aPathElements
+    )
+    aAttr['LocalPackagePath'] = os.path.join(
+        aAttr['LocalPackageFolder'],
+        strPackageName
+    )
+    aAttr['LocalSha1Path'] = aAttr['LocalPackagePath'] + '.sha1'
+
+    return aAttr
 
 
 def install_package(aCfg, aToolAttr):
-	if os.path.isdir(aToolAttr['LocalPackageFolder'])==False:
-		os.makedirs(aToolAttr['LocalPackageFolder'])
+    if os.path.isdir(aToolAttr['LocalPackageFolder']) is False:
+        os.makedirs(aToolAttr['LocalPackageFolder'])
 
-	if os.path.isdir(aToolAttr['PacketDepackPath'])==False:
-		os.makedirs(aToolAttr['PacketDepackPath'])
+    if os.path.isdir(aToolAttr['PacketDepackPath']) is False:
+        os.makedirs(aToolAttr['PacketDepackPath'])
 
-	# Is the package already downloaded?
-	# Both the package and the sha1 must exist.
-	bDownloadOk = os.path.isfile(aToolAttr['LocalPackagePath']) and os.path.isfile(aToolAttr['LocalSha1Path'])
-	
-	if bDownloadOk==True:
-		print 'The package was already downloaded, check the files.'
-		# Check the sha1 sum.
-		bDownloadOk = check_sha1_sum(aToolAttr['LocalSha1Path'], aToolAttr['LocalPackagePath'])
-		if bDownloadOk==True:
-			print 'The checksums match: OK!'
-		else:
-			print 'Checksum mismatch, discarding downloaded files!'
-			os.remove(aToolAttr['LocalPackagePath'])
-			os.remove(aToolAttr['LocalSha1Path'])
-		
-	if bDownloadOk==False:
-		print 'The package is not in the repository. It must be downloaded.'
-			
-		for strRepositoryUrl in aCfg['repositories']:
-			if strRepositoryUrl[-1]!='/':
-				strRepositoryUrl += '/'
-			print 'Trying repository at %s...' % strRepositoryUrl
-			strPackageUrl = strRepositoryUrl + '/'.join(aToolAttr['PathElements']) + '/' + aToolAttr['PackageName']
-			strSha1Url = strPackageUrl + '.sha1'
-				
-			bDownloadOk = download.download_to_file(strSha1Url, aToolAttr['LocalSha1Path'])
-			if bDownloadOk==True:
-				bDownloadOk = download.download_to_file(strPackageUrl, aToolAttr['LocalPackagePath'])
-				if bDownloadOk==True:
-					# Check the sha1 sum.
-					bDownloadOk = check_sha1_sum(aToolAttr['LocalSha1Path'], aToolAttr['LocalPackagePath'])
-					if bDownloadOk==True:
-						print 'The checksums match: OK!'
-						break
-					else:
-						print 'Checksum mismatch, discarding downloaded files!'
-						os.remove(aToolAttr['LocalPackagePath'])
-						os.remove(aToolAttr['LocalSha1Path'])
-		
-	if bDownloadOk==True:
-		# Unpack the archive.
-		print 'Unpacking...'
-		tArchive = tarfile.open(aToolAttr['LocalPackagePath'])
-		tArchive.extractall(aToolAttr['PacketDepackPath'])
-		tArchive.close()
-			
-		# Create the depack marker.
-		fMarker = open(aToolAttr['LocalMarkerPath'], 'w')
-		fMarker.close()
+    # Is the package already downloaded?
+    # Both the package and the sha1 must exist.
+    bDownloadOk = os.path.isfile(aToolAttr['LocalPackagePath']) and \
+        os.path.isfile(aToolAttr['LocalSha1Path'])
 
-	return bDownloadOk
+    if bDownloadOk is True:
+        print('The package was already downloaded, check the files.')
+        # Check the sha1 sum.
+        bDownloadOk = check_sha1_sum(
+            aToolAttr['LocalSha1Path'],
+            aToolAttr['LocalPackagePath']
+        )
+        if bDownloadOk is True:
+            print('The checksums match: OK!')
+        else:
+            print('Checksum mismatch, discarding downloaded files!')
+            os.remove(aToolAttr['LocalPackagePath'])
+            os.remove(aToolAttr['LocalSha1Path'])
 
+    if bDownloadOk is False:
+        print('The package is not in the repository. It must be downloaded.')
+
+        for strRepositoryUrl in aCfg['repositories']:
+            if strRepositoryUrl[-1] != '/':
+                strRepositoryUrl += '/'
+            print('Trying repository at %s...' % strRepositoryUrl)
+            strPackageUrl = strRepositoryUrl
+            strPackageUrl += '/'.join(aToolAttr['PathElements'])
+            strPackageUrl += '/' + aToolAttr['PackageName']
+            strSha1Url = strPackageUrl + '.sha1'
+
+            bDownloadOk = download.download_to_file(
+                strSha1Url,
+                aToolAttr['LocalSha1Path']
+            )
+            if bDownloadOk is True:
+                bDownloadOk = download.download_to_file(
+                    strPackageUrl,
+                    aToolAttr['LocalPackagePath']
+                )
+                if bDownloadOk is True:
+                    # Check the sha1 sum.
+                    bDownloadOk = check_sha1_sum(
+                        aToolAttr['LocalSha1Path'],
+                        aToolAttr['LocalPackagePath']
+                    )
+                    if bDownloadOk is True:
+                        print('The checksums match: OK!')
+                        break
+                    else:
+                        print(
+                            'Checksum mismatch, discarding downloaded files!'
+                        )
+                        os.remove(aToolAttr['LocalPackagePath'])
+                        os.remove(aToolAttr['LocalSha1Path'])
+
+    if bDownloadOk is True:
+        # Unpack the archive.
+        print('Unpacking...')
+        tArchive = tarfile.open(aToolAttr['LocalPackagePath'])
+        tArchive.extractall(aToolAttr['PacketDepackPath'])
+        tArchive.close()
+
+        # Create the depack marker.
+        fMarker = open(aToolAttr['LocalMarkerPath'], 'w')
+        fMarker.close()
+
+    return bDownloadOk
 
 
 atMachineFallbacks = dict({
-	# Linux 32 bit.
-	'i486': ['i486', 'i386'],
-	'i586': ['i586', 'i486', 'i386'],
-	'i686': ['i686', 'i586', 'i486', 'i386'],
-	'x86_64': ['x86_64', 'i686', 'i586', 'i486', 'i386'],
-
-	# Windows 64 bit.
-	'amd64': ['amd64', 'x86']
+    'x86': ['x86', 'i686', 'i586', 'i486', 'i386'],
+    'x86_64': ['x86_64', 'amd64', 'i686', 'i586', 'i486', 'i386']
 })
 
+
 def process_package(aCfg, aTool):
-	print 'Processing package %s, version %s' % (aTool['name'], aTool['version'])
+    print('Processing package %s, version %s' % (
+        aTool['name'],
+        aTool['version']
+    ))
 
-	# Nothing found yet.
-	aToolAttr = None
-	
-	# Does the package have a machine placeholder?
-	strPackage = aTool['package']
-	if string.find(strPackage, '${machine}') > -1:
-		# Get the machine name and possible alternatives.
-		strMachineName = platform.machine().lower()
-		if strMachineName in atMachineFallbacks:
-			astrMachines = atMachineFallbacks[strMachineName]
-		else:
-			astrMachines = [strMachineName]
-	
-		tTemplate = string.Template(strPackage)
+    # Nothing found yet.
+    aToolAttr = None
 
-		# Loop over all possible machine types.
-		for strMachineName in astrMachines:
-			# Substitute the machine name in the package name.
-			strPackageMachine = tTemplate.safe_substitute(dict({ 'machine': strMachineName }))
-			print 'Looking for installed package %s.' % strPackageMachine
-			
-			# Get all attributes for the tool.
-			aToolAttr = getToolAttributes(aCfg, aTool, strPackageMachine)
+    # Does the package have a machine placeholder?
+    strPackage = aTool['package']
+    if string.find(strPackage, '${machine}') > -1:
+        # Get the machine name and possible alternatives.
+        tPlatform = PlatformDetect()
+        tPlatform.detect()
+        strMachineName = tPlatform.strHostCpuArchitecture
+        if strMachineName in atMachineFallbacks:
+            astrMachines = atMachineFallbacks[strMachineName]
+        else:
+            astrMachines = [strMachineName]
 
-			# Check if this package name is installed.
-			if os.path.isfile(aToolAttr['LocalMarkerPath'])==True:
-				# Use this package.
-				print 'Found package!'
-				break
-			else:
-				aToolAttr = None
-		
-		if aToolAttr is None:
-			# Loop over all possible machine types.
-			for strMachineName in astrMachines:
-				# Substitute the machine name in the package name.
-				strPackageMachine = tTemplate.safe_substitute(dict({ 'machine': strMachineName }))
-				print 'Trying to install package %s.' % strPackageMachine
-			
-				# Get all attributes for the tool.
-				aToolAttr = getToolAttributes(aCfg, aTool, strPackageMachine)
+        tTemplate = string.Template(strPackage)
 
-				# Install package.
-				fResult = install_package(aCfg, aToolAttr)
-				if fResult==True:
-					print 'Installed package.'
-					break
-				else:
-					aToolAttr = None
-		
-	else:
-		# Get all attributes for the tool.
-		aToolAttr = getToolAttributes(aCfg, aTool, strPackage)
+        # Loop over all possible machine types.
+        for strMachineName in astrMachines:
+            # Substitute the machine name in the package name.
+            strPackageMachine = tTemplate.safe_substitute(
+                dict({'machine': strMachineName})
+            )
+            print('Looking for installed package %s.' % strPackageMachine)
 
-		# Check if this package name is installed.
-		if os.path.isfile(aToolAttr['LocalMarkerPath'])==True:
-			# Use this package.
-			print 'Found package!'
-		else:
-			fResult = install_package(aCfg, aToolAttr)
-			if fResult==True:
-				print 'Installed package.'
-			else:
-				aToolAttr = None
+            # Get all attributes for the tool.
+            aToolAttr = getToolAttributes(aCfg, aTool, strPackageMachine)
 
-	if aToolAttr is None:
-		raise Exception('Failed to install package %s, version %s' % (aTool['name'], aTool['version']))
-	
-	return aToolAttr
+            # Check if this package name is installed.
+            if os.path.isfile(aToolAttr['LocalMarkerPath']) is True:
+                # Use this package.
+                print('Found package!')
+                break
+            else:
+                aToolAttr = None
 
+        if aToolAttr is None:
+            # Loop over all possible machine types.
+            for strMachineName in astrMachines:
+                # Substitute the machine name in the package name.
+                strPackageMachine = tTemplate.safe_substitute(
+                    dict({'machine': strMachineName})
+                )
+                print('Trying to install package %s.' % strPackageMachine)
 
+                # Get all attributes for the tool.
+                aToolAttr = getToolAttributes(aCfg, aTool, strPackageMachine)
+
+                # Install package.
+                fResult = install_package(aCfg, aToolAttr)
+                if fResult is True:
+                    print('Installed package.')
+                    break
+                else:
+                    aToolAttr = None
+
+    else:
+        # Get all attributes for the tool.
+        aToolAttr = getToolAttributes(aCfg, aTool, strPackage)
+
+        # Check if this package name is installed.
+        if os.path.isfile(aToolAttr['LocalMarkerPath']) is True:
+            # Use this package.
+            print('Found package!')
+        else:
+            fResult = install_package(aCfg, aToolAttr)
+            if fResult is True:
+                print('Installed package.')
+            else:
+                aToolAttr = None
+
+    if aToolAttr is None:
+        raise Exception('Failed to install package %s, version %s' % (
+            aTool['name'],
+            aTool['version']
+        ))
+
+    return aToolAttr
